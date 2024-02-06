7ea653673fd386f1bcade624daa6c7f84abe2471
AlexandrLatushko <latushkoalex94@gmail.com>
2024-02-06 13:26:04 +0300

    add step 22

---
+++ script.js
@@ -0,0 +1,12 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+// Step 22
+// Use querySelector() to get the other two button elements using their ids: button2 and button3. 
+//  Store them in variables called button2 and button3. Remember to use const.
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
